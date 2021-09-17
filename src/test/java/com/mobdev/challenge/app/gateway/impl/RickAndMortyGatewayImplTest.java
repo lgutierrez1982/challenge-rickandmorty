32ca8f2a9ec402992ff780a557231f5877dd8933
lgutierrez1982 <lgutierrez1982@gmail.com>
2021-09-17 02:16:27 -0300

    remove unused import

--- src/test/java/com/mobdev/challenge/app/gateway/impl/RickAndMortyGatewayImplTest.java
+++ src/test/java/com/mobdev/challenge/app/gateway/impl/RickAndMortyGatewayImplTest.java
@@ -2,7 +2,6 @@ package com.mobdev.challenge.app.gateway.impl;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
